c7f6f5996d430a997779b6ea50bbb0a6f6f5b4f3
Nathan Peterson <nalexpeterson@gmail.com>
2016-11-29 16:09:03 -0800

    Add firebase Config file

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig ={
+  apiKey: "AIzaSyCqCrqFdEdUCi3-HhZrNl74GHNJ3KKc1qo",
+  authDomain: "angular-2-test-01-c89dd.firebaseapp.com",
+  databaseURL: "https://angular-2-test-01-c89dd.firebaseio.com",
+  storageBucket: "angular-2-test-01-c89dd.appspot.com",
+}
